c3f633cead3860a237eb301c6b09d6dc374d6333
Maxwell Barvian <max@barvian.me>
2015-10-20 08:21:06 -0500

    Hero

--- site/snippets/hero.php
+++ site/snippets/hero.php
@@ -3,8 +3,8 @@
     <h1 class="hero__text"><?php echo $hero['text'] ?></h1>
     <div class="hero__buttons">
       <?php foreach($hero['buttons'] as $button): ?>
-      <a href="<?php echo url($button['page']) ?>">
-        <?php echo $button['callout'] ?><br/>
+      <a href="<?php echo url($button['page']) ?>" class="hero__button">
+        <span class="hero__button__question"><?php echo $button['callout'] ?></span>
         <?php echo $button['action'] ?>.
       </a>
       <?php endforeach ?>
